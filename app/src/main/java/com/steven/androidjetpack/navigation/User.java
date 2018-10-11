eba84dc0ad4f4d4177e4287257c2c19ad8efa73e
zhiwenyan <zhiwenyan@icloud.com>
2018-10-11 17:22:14 +0800

    navigation

---
+++ app/src/main/java/com/steven/androidjetpack/navigation/User.java
@@ -0,0 +1,17 @@
+package com.steven.androidjetpack.navigation;
+
+/**
+ * Description:
+ * Data：10/11/2018-4:36 PM
+ *
+ * @author yanzhiwen
+ */
+public class User {
+    public String username;
+    public int avatar;
+
+    public User(String username, int avatar) {
+        this.username = username;
+        this.avatar = avatar;
+    }
+}
